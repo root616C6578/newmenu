e383c12df0e4ce102049dccd5dac7309aa50fa7e
root616C6578 <vitrivalexander@gmail.com>
2025-03-31 22:31:18 +0300

    ifka

--- menugui.py
+++ menugui.py
@@ -58,7 +58,6 @@ def scan_bluetooth_devices():
     mac_addresses = []  
     if len(nearby_devices) == 0:
         print("Не знайдено пристроїв.")
-        draw_image()
 
     if len(nearby_devices) > 0:
         print(f"Знайдено {len(nearby_devices)} пристроїв:")
@@ -236,54 +235,63 @@ while True:
             cursor = 0
             while True:
                 #draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
-                img = original_img.copy()
-                draw = ImageDraw.Draw(img)
-                y = 20
-                for i, addr in enumerate(mac_addresses):
-                    if i == cursor:
-                        draw.text((25, y), f"> {addr}", font=font, fill="white")
-                    else:
-                        draw.text((25, y), f"  {addr}", font=font, fill="white")
-                    y += 20
-                disp.display(img)
-                button_state_UP = GPIO.input(BUTTON_UP)
-                button_state_DOWN = GPIO.input(BUTTON_DOWN)
-                button_state_SELECT = GPIO.input(BUTTON_SELECT)
-                if button_state_UP == GPIO.LOW:
-                    cursor = (cursor - 1) % len(mac_addresses)
-                    time.sleep(0.1)
-                if button_state_DOWN == GPIO.LOW:
-                    cursor = (cursor + 1) % len(mac_addresses)
-                    time.sleep(0.1)
-
-                if button_state_SELECT == GPIO.LOW:
-                    selected_mac = mac_addresses[cursor]
-                    
-                    
-                    command = ["timeout", "1", "sudo", "l2ping", "-s", "600", "-f", selected_mac]
-                    print(f"l2ping {selected_mac}")               
-                    # subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) 
-                     
-                        
-                        
-                        
-                    # Вивести статус на екран
-                    #draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
+                if len(mac_addresses) == 0:
                     img = original_img.copy()
                     draw = ImageDraw.Draw(img)
-                    draw.text((25, 20), f"l2ping {selected_mac}", fill="white")
-                    for i in range(120):
-                        subprocess.run(command)
-                        #draw.rectangle((40, 30, 160, 50), outline="black", fill="black")
+                    draw.text((25, 20), "No devices found", font=font, fill="white")
+                    disp.display(img)
+                    time.sleep(2)
+                    break
+                else:
+                    img = original_img.copy()
+                    draw = ImageDraw.Draw(img)
+                    y = 20
+                    for i, addr in enumerate(mac_addresses):
+                        if i == cursor:
+                            draw.text((25, y), f"> {addr}", font=font, fill="white")
+                        else:
+                            draw.text((25, y), f"  {addr}", font=font, fill="white")
+                        y += 20
+                    disp.display(img)
+                    button_state_UP = GPIO.input(BUTTON_UP)
+                    button_state_DOWN = GPIO.input(BUTTON_DOWN)
+                    button_state_SELECT = GPIO.input(BUTTON_SELECT)
+                    if button_state_UP == GPIO.LOW:
+                        cursor = (cursor - 1) % len(mac_addresses)
+                        time.sleep(0.1)
+                    if button_state_DOWN == GPIO.LOW:
+                        cursor = (cursor + 1) % len(mac_addresses)
+                        time.sleep(0.1)
+
+                    if button_state_SELECT == GPIO.LOW:
+                        selected_mac = mac_addresses[cursor]
+                        
+                        
+                        command = ["timeout", "1", "sudo", "l2ping", "-s", "600", "-f", selected_mac]
+                        print(f"l2ping {selected_mac}")               
+                        # subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) 
+                        
+                            
+                            
+                            
+                        # Вивести статус на екран
+                        #draw.rectangle((0, 0, 160, 128), outline="black", fill="black")
                         img = original_img.copy()
                         draw = ImageDraw.Draw(img)
-                        draw.text((40, 30), f"at {i+1}", fill="white")
-                        disp.display(img)
-                     
-
-                    time.sleep(1)
-                    draw_image()
-                    
+                        draw.text((25, 20), f"l2ping {selected_mac}", fill="white")
+                        for i in range(120):
+                            subprocess.run(command)
+                            #draw.rectangle((40, 30, 160, 50), outline="black", fill="black")
+                            img = original_img.copy()
+                            draw = ImageDraw.Draw(img)
+                            draw.text((40, 30), f"at {i+1}", fill="white")
+                            disp.display(img)
+                        
+                        
+                        time.sleep(1)
+                        draw_image()
+                        break
+                        
             time.sleep(0.1)
             draw_image()
 
